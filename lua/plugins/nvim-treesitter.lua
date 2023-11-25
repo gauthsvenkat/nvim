dd4088d2b1a9ba24543eb14948409b4ae4dde87f
Gautham Venkataraman <gautham@dexterenergy.nl>
2023-11-25 11:36:43 +0100

    treesitter

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,14 @@
+-- TODO : figure out incremental selection
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = { "python", "c", "lua", "vim", "vimdoc", "query", "html" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
