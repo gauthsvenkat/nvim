c5fc50cb4068fd627f28e162afdc18538439a120
Gautham <gauthsvenkat@gmail.com>
2023-11-25 22:01:37 +0100

    tabs to spaces

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,29 +1,29 @@
 -- TODO : Integrate with LSP, DAP and whatnot
 return {
-	{
-		"williamboman/mason.nvim",
-		build = ":MasonUpdate",
-		opts = {
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		},
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		opts = { handlers = {
-			function(server_name)
-				require("lspconfig")[server_name].setup({})
-			end,
-		} },
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
-	},
+    {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate",
+        opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        },
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        opts = { handlers = {
+            function(server_name)
+                require("lspconfig")[server_name].setup({})
+            end,
+        } },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
+    },
 }
