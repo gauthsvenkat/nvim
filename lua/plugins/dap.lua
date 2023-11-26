da9492215dcc0bd3eee3de72ee757a1f03dea63d
Gautham <gauthsvenkat@gmail.com>
2023-11-26 22:59:45 +0100

    moving dependencies around

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,23 +1,25 @@
 return {
-    {
-        "rcarriga/nvim-dap-ui",
-        dependencies = { "mfussenegger/nvim-dap" },
-        config = true,
-    },
-    {
-        "theHamsta/nvim-dap-virtual-text",
-        dependencies = { "mfussenegger/nvim-dap" },
-        config = true,
-    },
-    {
-        "jay-babu/mason-nvim-dap.nvim",
-        dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
-        opts = {
-            handlers = {
-                function(config)
-                    require("mason-nvim-dap").default_setup(config)
-                end,
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = {
+        "williamboman/mason.nvim",
+        {
+            "rcarriga/nvim-dap-ui",
+            dependencies = {
+                "mfussenegger/nvim-dap",
+                {
+                    "theHamsta/nvim-dap-virtual-text",
+                    dependencies = { "mfussenegger/nvim-dap" },
+                    config = true,
+                },
             },
+            config = true,
+        },
+    },
+    opts = {
+        handlers = {
+            function(config)
+                require("mason-nvim-dap").default_setup(config)
+            end,
         },
     },
 }
--- lua/plugins/linting-formatting.lua
+++ lua/plugins/linting-formatting.lua
@@ -1,5 +1,8 @@
 return {
     "jay-babu/mason-null-ls.nvim",
-    dependencies = { "williamboman/mason.nvim", "nvimtools/none-ls.nvim" },
+    dependencies = {
+        "williamboman/mason.nvim",
+        { "nvimtools/none-ls.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
     config = true,
 }
