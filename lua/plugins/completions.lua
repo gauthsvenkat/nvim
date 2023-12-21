ca792be63999e4becbd28b3f70c6f7af7c9f681c
Gautham <gauthsvenkat@gmail.com>
2023-12-22 00:09:17 +0100

    fixed dumbass typo

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -17,8 +17,8 @@ return {
         "hrsh7th/nvim-cmp",
         dependencies = {
             "hrsh7th/cmp-buffer",
-            "hrsh6th/cmp-path",
-            "hrsh6th/cmp-cmdline",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
             "L3MON4D3/LuaSnip",
             "saadparwaiz1/cmp_luasnip",
             "zbirenbaum/copilot-cmp",
