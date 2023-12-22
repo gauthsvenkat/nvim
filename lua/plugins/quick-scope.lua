7400398b3e63fb944f6b4d1d3e8b7f77447bf20e
Gautham Venkataraman <gautham@dexterenergy.nl>
2023-12-22 15:22:40 +0100

    removed quick-scope infavor of hop

--- lua/plugins/quick-scope.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "unblevable/quick-scope",
-    -- config doesn't work for some reason but using init does
-    init = function()
-        vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
-    end,
-}
