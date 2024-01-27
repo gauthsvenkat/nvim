f22e96037ef5e550b51d10e5f5bd6f5e0412c987
Gautham Venkataraman <gautham@dexterenergy.nl>
2024-01-27 21:24:39 +0100

    ran pre-commit

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,7 +3,7 @@ local u = require("utils")
 return {
     "williamboman/mason.nvim",
     build = ":MasonUpdate",
-    lazy=false,
+    lazy = false,
     opts = {},
     keys = {
         { "<leader>pm", u._cmd("Mason"), desc = "Open Mason" },
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,7 @@ local u = require("utils")
 return {
     {
         "nvim-telescope/telescope-fzf-native.nvim",
-        build =
-        "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
     },
     {
         "nvim-telescope/telescope.nvim",
@@ -27,18 +26,18 @@ return {
             telescope.load_extension("fzf")
         end,
         keys = {
-            { "<leader>ff", u._cmd("Telescope find_files"),      desc = "Files" },
-            { "<leader>ft", u._cmd("Telescope live_grep"),       desc = "File with matching text" },
-            { "<leader>fs", u._cmd("Telescope grep_string"),     desc = "String in current working directory" },
-            { "<leader>fb", u._cmd("Telescope buffers"),         desc = "Buffer" },
+            { "<leader>ff", u._cmd("Telescope find_files"), desc = "Files" },
+            { "<leader>ft", u._cmd("Telescope live_grep"), desc = "File with matching text" },
+            { "<leader>fs", u._cmd("Telescope grep_string"), desc = "String in current working directory" },
+            { "<leader>fb", u._cmd("Telescope buffers"), desc = "Buffer" },
             { "<leader>fc", u._cmd("Telescope command_history"), desc = "Command in history" },
-            { "<leader>fh", u._cmd("Telescope search_history"),  desc = "Search history" },
-            { "<leader>fk", u._cmd("Telescope keymaps"),         desc = "(normal mode) keymaps" },
-            { "<leader>fv", u._cmd("Telescope treesitter"),      desc = "(treesitter) variables" },
-            { "<leader>gs", u._cmd("Telescope git_status"),      desc = "Git status" },
-            { "<leader>gS", u._cmd("Telescope git_stash"),       desc = "Git stash" },
-            { "<leader>gb", u._cmd("Telescope git_branches"),    desc = "Git branches" },
-            { "<leader>gc", u._cmd("Telescope git_commits"),     desc = "Git commits" },
+            { "<leader>fh", u._cmd("Telescope search_history"), desc = "Search history" },
+            { "<leader>fk", u._cmd("Telescope keymaps"), desc = "(normal mode) keymaps" },
+            { "<leader>fv", u._cmd("Telescope treesitter"), desc = "(treesitter) variables" },
+            { "<leader>gs", u._cmd("Telescope git_status"), desc = "Git status" },
+            { "<leader>gS", u._cmd("Telescope git_stash"), desc = "Git stash" },
+            { "<leader>gb", u._cmd("Telescope git_branches"), desc = "Git branches" },
+            { "<leader>gc", u._cmd("Telescope git_commits"), desc = "Git commits" },
         },
     },
 }
