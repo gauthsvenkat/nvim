57b4e2588ab3f1e1a4401c79b6807ed485575cbb
Gautham <gauthsvenkat@gmail.com>
2024-01-27 22:39:49 +0100

    refactor

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,113 +1,106 @@
 local u = require("utils")
 
 return {
-    {
-        "zbirenbaum/copilot.lua",
-        build = ":Copilot auth",
-        init = function()
-            u.register_group_with_whichkey("<leader>pc", "Copilot")
-        end,
-        opts = {
-            suggestion = { enabled = false },
-            panel = { enabled = false },
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        {
+            "zbirenbaum/copilot.lua",
+            build = ":Copilot auth",
+            init = function()
+                u.register_group_with_whichkey("<leader>pc", "Copilot")
+            end,
+            opts = {
+                suggestion = { enabled = false },
+                panel = { enabled = false },
+            },
+            keys = {
+                { "<leader>pcc", u._cmd("Copilot status"), desc = "Copilot status" },
+                { "<leader>pct", u._cmd("Copilot toggle"), desc = "Toggle Copilot" },
+                { "<leader>pcs", u._cmd("Copilot panel"), desc = "Open panel suggestions" },
+            },
         },
-        keys = {
-            { "<leader>pcc", u._cmd("Copilot status"), desc = "Copilot status" },
-            { "<leader>pct", u._cmd("Copilot toggle"), desc = "Toggle Copilot" },
-            { "<leader>pcs", u._cmd("Copilot panel"), desc = "Open panel suggestions" },
-        },
-    },
-    {
+        { "L3MON4D3/LuaSnip", dependencies = { "rafamadriz/friendly-snippets" }, build = "make install_jsregexp" },
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "saadparwaiz1/cmp_luasnip",
         "zbirenbaum/copilot-cmp",
-        dependencies = { "zbirenbaum/copilot.lua" },
-        opts = {},
+        "onsails/lspkind.nvim",
     },
-    { "L3MON4D3/LuaSnip", dependencies = { "rafamadriz/friendly-snippets" }, build = "make install_jsregexp" },
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-            "zbirenbaum/copilot-cmp",
-            "onsails/lspkind.nvim",
-        },
-        config = function()
-            -- setup autocompletion
-            require("luasnip/loaders/from_vscode").lazy_load()
+    config = function()
+        -- setup autocompletion
+        require("luasnip/loaders/from_vscode").lazy_load()
 
-            local luasnip = require("luasnip")
-            local cmp = require("cmp")
-            local lspkind = require("lspkind")
+        local luasnip = require("luasnip")
+        local cmp = require("cmp")
+        local lspkind = require("lspkind")
 
-            cmp.setup({
-                -- preselect = cmp.PreselectMode.None,
-                sources = cmp.config.sources({
-                    -- lspconfig is assumed to be setup
-                    { name = "nvim_lsp", keyword_length = 3 },
-                    { name = "luasnip", keyword_length = 3 },
-                    { name = "copilot", keyword_length = 1 },
-                }, {
-                    { name = "buffer" },
+        cmp.setup({
+            -- preselect = cmp.PreselectMode.None,
+            sources = cmp.config.sources({
+                -- lspconfig is assumed to be setup
+                { name = "nvim_lsp", keyword_length = 3 },
+                { name = "luasnip", keyword_length = 3 },
+                { name = "copilot", keyword_length = 1 },
+            }, {
+                { name = "buffer" },
+            }),
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            }),
+            formatting = {
+                format = lspkind.cmp_format({
+                    mode = "text_symbol",
+                    maxwidth = 50,
+                    ellipsis_char = "...",
+                    symbol_map = { Copilot = "" },
                 }),
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                }),
-                formatting = {
-                    format = lspkind.cmp_format({
-                        mode = "text_symbol",
-                        maxwidth = 50,
-                        ellipsis_char = "...",
-                        symbol_map = { Copilot = "" },
-                    }),
-                },
-                experimental = {
-                    ghost_text = true,
-                },
-            })
-            -- use buffer source for ? and / search
-            cmp.setup.cmdline({ "/", "?" }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = { {
-                    name = "buffer",
-                } },
-            })
-            -- user cmdline source for : commands
-            cmp.setup.cmdline(":", {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = "path" },
-                }, {
-                    { name = "cmdline" },
-                }),
-            })
-        end,
-    },
+            },
+            experimental = {
+                ghost_text = true,
+            },
+        })
+        -- use buffer source for ? and / search
+        cmp.setup.cmdline({ "/", "?" }, {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = { {
+                name = "buffer",
+            } },
+        })
+        -- user cmdline source for : commands
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = "path" },
+            }, {
+                { name = "cmdline" },
+            }),
+        })
+    end,
 }
--- lua/plugins/linting_and_formatting.lua
+++ lua/plugins/linting_and_formatting.lua
@@ -2,20 +2,23 @@ local u = require("utils")
 
 return {
     "jay-babu/mason-null-ls.nvim",
-    lazy = false,
+    event = "BufEnter",
     dependencies = {
+        {
+            "nvimtools/none-ls.nvim",
+            init = function()
+                u.register_group_with_whichkey("<leader>pn", "NullLs")
+            end,
+            opts = {
+                fallback_severity = vim.diagnostic.severity.INFO,
+            },
+        },
         "williamboman/mason.nvim",
-        "nvimtools/none-ls.nvim",
         "nvim-lua/plenary.nvim",
     },
-    init = function()
-        u.register_group_with_whichkey("<leader>pn", "NullLs")
-    end,
-    config = function()
-        require("null-ls").setup({ fallback_severity = vim.diagnostic.severity.INFO })
-        -- this will cause all sources to be registered with null-ls
-        require("mason-null-ls").setup({ handlers = {} })
-    end,
+    opts = {
+        handlers = {},
+    },
     keys = {
         { "<leader>pnn", u._cmd("NullLsInfo"), desc = "Show NullLs info" },
         { "<leader>pnl", u._cmd("NullLsLog"), desc = "Show NullLs logs" },
