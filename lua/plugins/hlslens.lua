09eb9f105eb576829c74feab695f3256771d7eb5
Gautham <gauthsvenkat@gmail.com>
2024-10-01 20:52:50 +0200

    surround and sleuth

--- lua/plugins/hlslens.lua
+++ lua/plugins/hlslens.lua
@@ -1,6 +1,6 @@
 return {
   "kevinhwang91/nvim-hlslens",
-  opts = { calm_down = true },
+  wpts = { calm_down = true },
   keys = {
     { "/", mode = { "n", "v" } },
     { "?", mode = { "n", "v" } },
