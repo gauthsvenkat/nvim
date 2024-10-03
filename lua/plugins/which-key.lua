17d385eeebdd0ce0f0624c30a723a8e1389bfd1f
Gautham <gauthsvenkat@gmail.com>
2024-10-03 07:22:26 +0200

    More keys

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -12,8 +12,7 @@ return {
       { "<C-l>", "<C-w>l", desc = "Right window" },
 
       { "<C-q>", group = "Close" },
-      { "<C-q>q", cmd("close"), desc = "Close window" },
-      { "<C-q>b", cmd("bd"), desc = "Close buffer" },
+      { "<C-q>q", cmd("bd"), desc = "Close buffer" },
 
       { "<C-n>", cmd("bnext"), desc = "Next buffer" },
       { "<C-b>", cmd("bprevious"), desc = "Previous buffer" },
@@ -23,6 +22,15 @@ return {
       { "<C-s>a", cmd("aboveleft vsplit"), desc = "Split vertical (left)" },
       { "<C-s>s", cmd("belowright split"), desc = "Split horizontal (below)" },
       { "<C-s>d", cmd("belowright vsplit"), desc = "Split vertical (right)" },
+
+      { "<leader>w", group = "Write" },
+      { "<leader>ww", cmd("write"), desc = "Write buffer" },
+      { "<leader>wa", cmd("wall"), desc = "Write all buffers" },
+      { "<leader>wq", cmd("wq"), desc = "Write buffer and quit" },
+
+      { "<leader>q", group = "Quit" },
+      { "<leader>qq", cmd("quit"), desc = "Quit window" },
+      { "<leader>qa", cmd("qall"), desc = "Quit all" },
     },
   },
 }
