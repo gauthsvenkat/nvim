bfc3ba29260a84de584c93f983db25bc36b75986
Gautham <gauthsvenkat@gmail.com>
2024-10-05 19:02:48 +0200

    Indent blankline

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,12 @@
+local cmd = require("utils").cmd
+
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  event = "VeryLazy",
+  opts = {},
+  main = "ibl",
+  keys = {
+    { "<leader>ii", cmd("IBLToggle"), desc = "Indent Blankline toggle" },
+    { "<leader>is", cmd("IBLToggleScope"), desc = "Indent Blankline scope toggle" },
+  },
+}
