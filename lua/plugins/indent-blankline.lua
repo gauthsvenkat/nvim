c2bf2b4334f49ba40911e7790d3061121f78a103
Gautham Venkataraman <gautham@dexterenergy.ai>
2024-10-08 18:37:41 +0200

    Make indent blankline play nice

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,7 +3,21 @@ local cmd = require("utils").cmd
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = "VeryLazy",
-  opts = { enabled = false },
+  opts = {
+    exclude = {
+      filetypes = {
+        "lspinfo",
+        "dashboard",
+        "checkhealth",
+        "help",
+        "man",
+        "gitcommit",
+        "TelescopePrompt",
+        "TelescopeResults",
+        "",
+      },
+    },
+  },
   main = "ibl",
   keys = {
     { "<leader>ii", cmd("IBLToggle"), desc = "Indent Blankline toggle" },
