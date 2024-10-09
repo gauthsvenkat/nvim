f7b32d8ec963dc7d1a0c325a5286cd0b4c56b96b
Gautham <gauthsvenkat@gmail.com>
2024-10-09 22:44:20 +0200

    Oil but better

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,6 +4,17 @@ return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
+    win_options = {
+      wrap = true,
+    },
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,
+      is_always_hidden = function(name, _)
+        return name == ".."
+      end,
+    },
     keymaps = {
       ["<BS>"] = "actions.parent",
       ["<Esc>"] = "actions.close",
