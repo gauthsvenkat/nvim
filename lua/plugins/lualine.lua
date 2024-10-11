6959b5dcf96fc0de7bd85f59d29632587c60fd81
Gautham <gauthsvenkat@gmail.com>
2024-10-11 06:37:55 +0200

    lualine update

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,12 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "arkav/lualine-lsp-progress",
+  },
   opts = {
     sections = {
-      lualine_c = {},
+      lualine_c = { "lsp_progress" },
       lualine_x = {},
       lualine_y = { "buffers" },
     },
