4f328de8fa96029e928519ccc01a98f6da50b5a2
Gautham <gauthsvenkat@gmail.com>
2024-10-14 21:39:10 +0200

    better color

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,7 +3,19 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    config = function()
+    opts = {
+      flavour = "mocha",
+      dim_inactive = { enabled = false, percentage = 0.10 },
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
       vim.cmd([[colorscheme catppuccin]])
     end,
   },
