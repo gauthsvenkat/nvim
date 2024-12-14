9b698e6a38c5358b2a9df94b3be7987f29cc7c45
Gautham Venkataraman <gauthsvenkat@gmail.com>
2024-12-14 14:54:16 +0100

    add keymaps for scaling neovide

--- init.lua
+++ init.lua
@@ -9,7 +9,6 @@ require("config.options")
 require("config.lazy")
 
 -- Highlight on yank
-
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("highlight_yank", {}),
   desc = "Hightlight selection on yank",
@@ -18,3 +17,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
   end,
 })
+
+-- Neovide
+if vim.g.neovide then
+  vim.g.neovide_scale_factor = 0.9
+
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.15)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.15)
+  end)
+end
