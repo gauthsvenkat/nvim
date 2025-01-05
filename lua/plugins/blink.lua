b1d43313543ae6c9ee22af15f7b669b76813bc6d
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-01-05 16:05:51 +0100

    can't live without copilot

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,16 @@
 return {
   "saghen/blink.cmp",
-  dependencies = "rafamadriz/friendly-snippets",
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    {
+      "giuxtaposition/blink-cmp-copilot",
+      dependencies = {
+        "zbirenbaum/copilot.lua",
+        build = ":Copilot auth",
+        opts = { suggestion = { enabled = false }, panel = { enabled = false } },
+      },
+    },
+  },
   version = "*",
   opts = {
     keymap = { preset = "default" },
@@ -8,8 +18,36 @@ return {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
     },
+    -- NOTE: Following section copied from lazyvim.
+    completion = {
+      accept = {
+        auto_brackets = {
+          enabled = true,
+        },
+      },
+      menu = {
+        draw = {
+          treesitter = { "lsp" },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+      ghost_text = {
+        enabled = false,
+      },
+    },
     sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "lsp", "path", "snippets", "buffer", "copilot" },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
+        },
+      },
     },
   },
   opts_extend = { "sources.default" },
