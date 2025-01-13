db6d764478cef0cc8c475b00b00d965d1c46a1e4
Gautham Venkataraman <gautham@dexterenergy.ai>
2025-01-13 11:58:37 +0530

    treewalker only if parser available

--- lua/plugins/treewalker.lua
+++ lua/plugins/treewalker.lua
@@ -1,31 +1,43 @@
 local cmd = require("utils").cmd
 
--- TODO: Only enable keybinds if there is a parser for the buffer
 return {
   "aaronik/treewalker.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "folke/which-key.nvim",
+  },
   opts = {
     highlight = true,
     highlight_duration = 250,
   },
+  config = function(_, opts)
+    local wk = require("which-key")
+    local buffer = vim.api.nvim_get_current_buf()
+
+    require("treewalker").setup(opts)
+
+    if require("nvim-treesitter.parsers").has_parser() then
+      wk.add({ "<Left>", cmd("Treewalker Left"), buffer = buffer })
+      wk.add({ "<Down>", cmd("Treewalker Down"), buffer = buffer })
+      wk.add({ "<Up>", cmd("Treewalker Up"), buffer = buffer })
+      wk.add({ "<Right>", cmd("Treewalker Right"), buffer = buffer })
+    end
+  end,
   keys = {
     {
       "<Left>",
-      cmd("Treewalker Left"),
       desc = "Treewalker left",
     },
     {
       "<Down>",
-      cmd("Treewalker Down"),
       desc = "Treewalker down",
     },
     {
       "<Up>",
-      cmd("Treewalker Up"),
       desc = "Treewalker up",
     },
     {
       "<Right>",
-      cmd("Treewalker Right"),
       desc = "Treewalker right",
     },
   },
