0d6efa2f257fdb760c8850703f3f871fde7aff16
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-06-08 16:35:59 +0200

    format on save toggle

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,21 +1,48 @@
 return {
   "stevearc/conform.nvim",
   dependencies = { "WhoIsSethDaniel/mason-tool-installer.nvim" },
-  event = { "BufWritePre" },
-  opts = {
-    -- NOTE: Ensure formatters are installed by adding them to mason.lua
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = {
-        "ruff_format",
-        "ruff_organize_imports",
+  event = { "InsertEnter" },
+  opts = function()
+    -- commands to enable or disable autoformat
+    vim.api.nvim_create_user_command("FormatDisable", function()
+      vim.g.disable_autoformat = true
+    end, { desc = "Disable autoformat-on-save" })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.g.disable_autoformat = false
+    end, { desc = "Enable autoformat_on_save" })
+
+    -- toggle keybind
+    Snacks.toggle
+      .new({
+        id = "autoformat_on_save",
+        name = "auto[f]ormat",
+        get = function()
+          return not vim.g.disable_autoformat
+        end,
+        set = function(enabled)
+          vim.g.disable_autoformat = not enabled
+        end,
+      })
+      :map("<leader>tf")
+
+    return {
+      -- NOTE: Ensure formatters are installed by adding them to mason.lua
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = {
+          "ruff_format",
+          "ruff_organize_imports",
+        },
+        rust = { "rustfmt" },
+        nix = { "alejandra" },
       },
-      rust = { "rustfmt" },
-      nix = { "alejandra" },
-    },
-    -- TODO: Format on save. See https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
-    -- Might be worth figuring out snacks toggle before tackling the above
-    format_on_save = { lsp_format = "fallback" },
-  },
+      format_on_save = function()
+        if not vim.g.disable_autoformat then
+          return { lsp_format = "fallback" }
+        end
+      end,
+    }
+  end,
   cmd = { "ConformInfo" },
 }
