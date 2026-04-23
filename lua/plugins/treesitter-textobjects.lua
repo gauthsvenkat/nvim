50da9130be151e627d3b1f30c720ecb9f2971859
Gautham Venkataraman <gauthsvenkat@gmail.com>
2026-04-23 19:52:12 +0200

    fix: migrate treesitter plugins to main branch for nvim 0.12
    The old master branch of nvim-treesitter is archived and incompatible
    with Neovim 0.12. Switch both nvim-treesitter and
    nvim-treesitter-textobjects to the main branch rewrite.

--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -1,89 +1,68 @@
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
+  branch = "main",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  main = "nvim-treesitter.configs",
-  opts = {
-    textobjects = {
-      move = {
-        enable = true,
-        goto_next_start = {
-          ["]a"] = { query = "@parameter.outer", desc = "Next [a]rgument start" },
-          ["]c"] = { query = "@class.outer", desc = "Next [c]lass start" },
-          ["]f"] = { query = "@function.outer", desc = "Next [f]unction start" },
-          ["]i"] = { query = "@conditional.outer", desc = "Next [i]f start" },
-          ["]l"] = { query = "@loop.outer", desc = "Next [l]oop start" },
-          ["]o"] = { query = "@comment.outer", desc = "Next c[o]mment start" },
-          ["]r"] = { query = "@return.outer", desc = "Next [r]eturn start" },
-        },
-        goto_next_end = {
-          ["]A"] = { query = "@parameter.outer", desc = "Next [A]rgument end" },
-          ["]C"] = { query = "@class.outer", desc = "Next [C]lass end" },
-          ["]F"] = { query = "@function.outer", desc = "Next [F]unction end" },
-          ["]I"] = { query = "@conditional.outer", desc = "Next [I]f end" },
-          ["]L"] = { query = "@loop.outer", desc = "Next [L]oop end" },
-          ["]O"] = { query = "@comment.outer", desc = "Next c[o]mment end" },
-          ["]R"] = { query = "@return.outer", desc = "Next [R]eturn end" },
-        },
-        goto_previous_start = {
-          ["[a"] = { query = "@parameter.outer", desc = "Previous [a]rgument start" },
-          ["[c"] = { query = "@class.outer", desc = "Previous [c]lass start" },
-          ["[f"] = { query = "@function.outer", desc = "Previous [f]unction start" },
-          ["[i"] = { query = "@conditional.outer", desc = "Previous [i]f start" },
-          ["[l"] = { query = "@loop.outer", desc = "Previous [l]oop start" },
-          ["[o"] = { query = "@comment.outer", desc = "Previous c[o]mment start" },
-          ["[r"] = { query = "@return.outer", desc = "Previous [r]eturn start" },
-        },
-        goto_previous_end = {
-          ["[A"] = { query = "@parameter.outer", desc = "Previous [A]rgument end" },
-          ["[C"] = { query = "@class.outer", desc = "Previous [C]lass end" },
-          ["[F"] = { query = "@function.outer", desc = "Previous [F]unction end" },
-          ["[I"] = { query = "@conditional.outer", desc = "Previous [I]f end" },
-          ["[L"] = { query = "@loop.outer", desc = "Previous [L]oop end" },
-          ["[O"] = { query = "@comment.outer", desc = "Previous c[o]mment end" },
-          ["[R"] = { query = "@return.outer", desc = "Previous [R]eturn end" },
-        },
-      },
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ["aa"] = { query = "@parameter.outer", desc = "[a]rgument" },
-          ["ia"] = { query = "@parameter.inner", desc = "[a]rgument" },
+  config = function()
+    require("nvim-treesitter-textobjects").setup({
+      select = { lookahead = true },
+    })
 
-          ["ac"] = { query = "@class.outer", desc = "[c]lass" },
-          ["ic"] = { query = "@class.inner", desc = "[c]lass" },
+    local select_to = require("nvim-treesitter-textobjects.select").select_textobject
+    local move = require("nvim-treesitter-textobjects.move")
+    local swap = require("nvim-treesitter-textobjects.swap")
 
-          ["af"] = { query = "@function.outer", desc = "[f]unction" },
-          ["if"] = { query = "@function.inner", desc = "[f]unction" },
+    local objects = {
+      { key = "a", query = "@parameter", desc = "[a]rgument" },
+      { key = "c", query = "@class", desc = "[c]lass" },
+      { key = "f", query = "@function", desc = "[f]unction" },
+      { key = "i", query = "@conditional", desc = "[i]f" },
+      { key = "l", query = "@loop", desc = "[l]oop" },
+      { key = "o", query = "@comment", desc = "c[o]mment" },
+      { key = "r", query = "@return", desc = "[r]eturn" },
+    }
 
-          ["ai"] = { query = "@conditional.outer", desc = "[i]f" },
-          ["ii"] = { query = "@conditional.inner", desc = "[i]f" },
+    for _, obj in ipairs(objects) do
+      local k, q, d = obj.key, obj.query, obj.desc
 
-          ["al"] = { query = "@loop.outer", desc = "[l]oop" },
-          ["il"] = { query = "@loop.inner", desc = "[l]oop" },
+      -- Select
+      vim.keymap.set({ "x", "o" }, "a" .. k, function()
+        select_to(q .. ".outer", "textobjects")
+      end, { desc = d })
 
-          ["ao"] = { query = "@comment.outer", desc = "c[o]mment" },
-          ["io"] = { query = "@comment.inner", desc = "c[o]mment" },
+      vim.keymap.set({ "x", "o" }, "i" .. k, function()
+        select_to(q .. ".inner", "textobjects")
+      end, { desc = d })
 
-          ["ar"] = { query = "@return.outer", desc = "[r]eturn" },
-          ["ir"] = { query = "@return.inner", desc = "[r]eturn" },
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ["gwa"] = { query = "@parameter.inner", desc = "next [a]rgument" },
-          ["gwc"] = { query = "@class.outer", desc = "next [c]lass" },
-          ["gwf"] = { query = "@function.outer", desc = "next [f]unction" },
-          ["gwi"] = { query = "@conditional.outer", desc = "next [i]f" },
-        },
-        swap_previous = {
-          ["gWa"] = { query = "@parameter.inner", desc = "prev [a]rgument" },
-          ["gWc"] = { query = "@class.outer", desc = "prev [c]lass" },
-          ["gWf"] = { query = "@function.outer", desc = "prev [f]unction" },
-          ["gWi"] = { query = "@conditional.outer", desc = "prev [i]f" },
-        },
-      },
-    },
-  },
+      -- Move
+      vim.keymap.set({ "n", "x", "o" }, "]" .. k, function()
+        move.goto_next_start(q .. ".outer", "textobjects")
+      end, { desc = "Next " .. d .. " start" })
+      vim.keymap.set({ "n", "x", "o" }, "]" .. k:upper(), function()
+        move.goto_next_end(q .. ".outer", "textobjects")
+      end, { desc = "Next " .. d .. " end" })
+      vim.keymap.set({ "n", "x", "o" }, "[" .. k, function()
+        move.goto_previous_start(q .. ".outer", "textobjects")
+      end, { desc = "Prev " .. d .. " start" })
+      vim.keymap.set({ "n", "x", "o" }, "[" .. k:upper(), function()
+        move.goto_previous_end(q .. ".outer", "textobjects")
+      end, { desc = "Prev " .. d .. " end" })
+    end
+
+    -- Swap (subset of objects)
+    local swap_objects = {
+      { key = "a", query = "@parameter.inner", desc = "[a]rgument" },
+      { key = "c", query = "@class.outer", desc = "[c]lass" },
+      { key = "f", query = "@function.outer", desc = "[f]unction" },
+      { key = "i", query = "@conditional.outer", desc = "[i]f" },
+    }
+
+    for _, obj in ipairs(swap_objects) do
+      vim.keymap.set("n", "gw" .. obj.key, function()
+        swap.swap_next(obj.query)
+      end, { desc = "next " .. obj.desc })
+      vim.keymap.set("n", "gW" .. obj.key, function()
+        swap.swap_previous(obj.query)
+      end, { desc = "prev " .. obj.desc })
+    end
+  end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   lazy = false,
   build = ":TSUpdate",
-  opts = {
-    -- Parsers that should always be installed
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-    -- Auto install missing parsers when entering a buffer
-    auto_install = true,
-
-    highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = false,
-        node_incremental = ";",
-        node_decremental = ",",
-      },
-    },
-  },
+  config = function()
+    require("nvim-treesitter").install({ "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" })
+  end,
 }
